99748f8580da5ef1444ee90c884b7df9f28c13a6
Pedro Augusto Sousa Gonçalves <pedro.asg@aluno.ufop.edu.br>
2026-02-17 12:47:57 -0300

    Solve nvim haskell problems

--- nvim/lua/plugins/haskell.lua
+++ nvim/lua/plugins/haskell.lua
@@ -1,28 +1,41 @@
 return {
   {
+    "neovimhaskell/haskell-vim",
+    lazy = false,
+    config = function()
+      vim.g.haskell_enable_quantification = 1
+      vim.g.haskell_enable_recursivedo = 1
+      vim.g.haskell_enable_arrowsyntax = 1
+      vim.g.haskell_enable_pattern_synonyms = 1
+      vim.g.haskell_enable_typeroles = 1
+      vim.g.haskell_enable_static_pointers = 1
+      vim.g.haskell_backpack = 1
+
+      vim.filetype.add({
+        extension = {
+          x = "haskell",
+          y = "haskell",
+          ly = "haskell",
+        },
+      })
+    end,
+  },
+
+  {
     "mrcjkb/haskell-tools.nvim",
     version = "^4", 
-    lazy = false, -- Importante para detectar arquivos ao abrir
+    lazy = false,
     
     config = function()
-      -- 1. Configurar a variável Global ANTES de carregar ferramentas internas
       vim.g.haskell_tools = {
         tools = {
-          -- REMOVIDO: inlay_hints (causava o erro)
-          -- REMOVIDO: codeLens (autoRefresh já é true por padrão)
-          
-          hover = { 
-            auto_focus = false 
-          },
+          hover = { auto_focus = false },
         },
         
         hls = {
           debug = false,
-          
-          -- IMPORTANTE: Passa as capacidades do Blink/CMP para o HLS
           capabilities = require('blink.cmp').get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities()),
 
-          -- Esta função roda APENAS quando o LSP conecta com sucesso
           on_attach = function(client, bufnr)
             local ht = require('haskell-tools')
             local opts = { noremap = true, silent = true, buffer = bufnr }
@@ -32,28 +45,22 @@ return {
               vim.keymap.set(mode, lhs, rhs, opts)
             end
 
-            -- === ATIVAR INLAY HINTS (Nativo do Neovim) ===
-            -- Substitui a config antiga que dava erro
-            if client.server_capabilities.inlayHintProvider then
+            -- === PROTEÇÃO CONTRA ERROS EM .X / .Y / .CABAL ===
+            local filename = vim.api.nvim_buf_get_name(bufnr)
+            local is_real_haskell = string.match(filename, "%.hs$") or string.match(filename, "%.lhs$")
+
+            if is_real_haskell and client.server_capabilities.inlayHintProvider then
               vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
             end
 
-            -- === HASKELL CLUSTER (<leader>h...) ===
-            
-            -- Code Actions (Essenciais)
+            -- Atalhos (Haskell Cluster)
             map('n', '<leader>ha', vim.lsp.buf.code_action, "Haskell: Code Actions")
             map('n', '<leader>hc', vim.lsp.codelens.run,    "Haskell: Run CodeLens")
-            
-            -- Hoogle & Docs
             map('n', '<leader>hs', ht.hoogle.hoogle_signature, "Haskell: Search Signature")
             map('n', '<leader>ht', vim.lsp.buf.hover,          "Haskell: Show Type/Hover")
-            
-            -- REPL (GHCi)
             map('n', '<leader>hr', ht.repl.toggle, "Haskell: Toggle REPL")
             map('n', '<leader>hf', function() ht.repl.toggle(vim.api.nvim_buf_get_name(0)) end, "Haskell: Load Buffer in REPL")
             map('n', '<leader>hq', ht.repl.quit,   "Haskell: Quit REPL")
-
-            -- Navegação e Avaliação
             map('n', '<leader>he', ht.lsp.buf_eval_all,    "Haskell: Evaluate Buffer")
             map('n', '<leader>hd', vim.lsp.buf.definition, "Haskell: Go to Definition")
           end,
